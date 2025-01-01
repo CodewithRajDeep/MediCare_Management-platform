0e1ef16d2f1d8e524f7991353aee0384615c0499
Deep Raj <deepraj6897@gmail.com>
2025-01-01 10:30:59 +0530

    Setup for Header Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "MediCare: Research Discovery Management Platform",
+  description: "A scalable, secure drug discovery management platform leveraging NEXT.js, MongoDB, NVIDIA models, and SMILE technology for molecule generation, user collaboration, and real-time insights for MediCare.",
+};
 
 export default function Home() {
   return (
